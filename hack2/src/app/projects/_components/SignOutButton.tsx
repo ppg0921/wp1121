7fed26009926ed477d879aadb14ee34e654ee4c6
betty <bettycheng0921@gmail.com>
2023-11-23 10:12:21 +0800

    ouo

---
+++ hack2/src/app/projects/_components/SignOutButton.tsx
@@ -0,0 +1,11 @@
+// TODO: 4. Call the signOut() function when the button is clicked
+// hint: You may want to change the first line of this file
+"use client"
+import { signOut } from "next-auth/react";
+
+import { Button } from "@/components/ui/button";
+
+export default function SignOutButton() {
+  return <Button data-testid="sign-out-button" variant={"outline"} onClick={()=>signOut}>Sign Out</Button>;
+}
+// TODO: 4. end
---
+++ hack2/src/lib/env/private.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+const privateEnvSchema = z.object({
+  // TODO: 1.2 Add your private environment variables here for your database (postgres)
+  // TODO: 1.2 end
+  POSTGRES_URL: z.string().url(),
+  PUSHER_ID: z.string(),
+  PUSHER_SECRET: z.string(),
+});
+
+type PrivateEnv = z.infer<typeof privateEnvSchema>;
+
+export const privateEnv: PrivateEnv = {
+  // TODO: 1.3 Add your private environment variables here for your database (postgres)
+  // TODO: 1.3 end
+  POSTGRES_URL: process.env.POSTGRES_URL!,
+  PUSHER_ID: process.env.PUSHER_ID!,
+  PUSHER_SECRET: process.env.PUSHER_SECRET!,
+};
+
+privateEnvSchema.parse(privateEnv);
